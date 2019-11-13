3aa61485a92cab04dc346ae8d01dd59129cda7e6
Corey Szeto <szetoc@mcmaster.ca>
2019-11-13 16:51:50 -0500

    Player Health and Death

---
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour {
+
+    private float maxHP = 200;
+    private float currentHp;
+
+    private bool isHealing;
+    private bool invincible;
+
+    private float invincibilityTime = 3;
+    private float invincibilityExpiry;
+    private float canHealTime;
+
+    private float amountToHeal;
+
+
+    // Use this for initialization
+    void Start() {
+        currentHp = maxHP;
+        isHealing = false;
+        invincible = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+       
+        if (currentHp < maxHP & canHealTime < Time.time)
+        {
+            amountToHeal = maxHP - currentHp;
+            healPlayer(amountToHeal);
+            Debug.Log(currentHp);
+        }
+        if (invincibilityExpiry < Time.time)
+        {
+            invincible = false;
+        }
+        else if (currentHp > maxHP)
+        {
+            currentHp = maxHP;
+
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" & invincible == false)
+        {
+            currentHp = currentHp - 100;
+            Debug.Log(currentHp);
+            invincible = true;
+            invincibilityExpiry = Time.time + invincibilityTime;
+            canHealTime = invincibilityExpiry + 6;
+        }
+        if (currentHp < 1)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    private void healPlayer(float amount)
+    {
+        currentHp += amount;
+    }
+
+
+
+}
+     
