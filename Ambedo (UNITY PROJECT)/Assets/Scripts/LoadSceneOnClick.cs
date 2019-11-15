6c9ac4159d9ec348dafb8cb9409e9da0032d89d2
Alex Pattison <alex.pattison77@gmail.com>
2019-11-15 16:02:41 -0500

    Main Menu scene with menu asset and fade asset

---
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex) {
+
+        Initiate.Fade("Demo Room 2", Color.black, 1.0f);
+    }
+
+}
