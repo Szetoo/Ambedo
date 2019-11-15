4313a700b971352cfa1f3238010afc0d1f168317
Corey Szeto <szetoc@mcmaster.ca>
2019-11-15 16:28:48 -0500

    save code

--- Ambedo (UNITY PROJECT)/Assets/Scripts/Game.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/Game.cs
@@ -1,16 +1,115 @@
-﻿using System.Collections;
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
+    public List<GameObject> bullets = new List<GameObject>();
+    public float xSpawnPosition;
+    public float ySpawnPosition;
+    GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Awake()
+    {
+       
+    }
+
+       
+  
+
+
+
+
+
+    public void SaveGame()
+    {
+        // 1
+        Save save = CreateSaveGameObject();
+
+        // 2
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/gamesave.save");
+        bf.Serialize(file, save);
+        file.Close();
+
+        // 3
+        xSpawnPosition = 0;
+        ySpawnPosition = 0;
+
+        Debug.Log(Application.persistentDataPath);
+        Debug.Log("Game Saved");
+    }
+
+
+    private Save CreateSaveGameObject()
+    {
+        Save save = new Save();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        save.xSpawnPosition = player.GetComponent<Transform>().position.x;
+        save.ySpawnPosition = player.GetComponent<Transform>().position.y;
+
+        return save;
+    }
+    /*public void NewGame()
+    {
+        hits = 0;
+        shots = 0;
+        shotsText.text = "Shots: " + shots;
+        hitsText.text = "Hits: " + hits;
+
+        ClearRobots();
+        ClearBullets();
+        RefreshRobots();
+
+        Unpause();
+    }*/
+
+
+
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void SaveAsJSON()
+    {
+
+    }
 }
