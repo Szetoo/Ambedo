e3b15c4d335866b2a7897ba5d4f622811069486f
Thomas Dykstra <dykstrat@mcmaster.ca>
2019-11-21 15:36:53 -0500

    added attacking capabilities

--- Ambedo (UNITY PROJECT)/Assets/Scripts/PlayerMovementController.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/PlayerMovementController.cs
@@ -2,166 +2,209 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PlayerMovementController : MonoBehaviour
+{
 
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+    public float xSpeed;
+    public float jumpForce;
+    public float accelSlowdown;
+    public float landingTolerance;
+    public float sprintMultiplier;
+    public int waterJumpFrames;
 
-public class PlayerMovementController : MonoBehaviour {
+    public GameObject hitbox;
 
-	public float xSpeed;
-	public float jumpForce;
-	public float accelSlowdown;
-	public float landingTolerance;
-	public float sprintMultiplier;
-	public int waterJumpFrames;
-
-	private bool isJumping;
-	private Vector2 lastVelocity;
-	private bool canStartSprint;
-	private bool isInWater;
-	private float nextWaterJump;
+    private bool isJumping;
+    private Vector2 lastVelocity;
+    private bool canStartSprint;
+    private bool isInWater;
+    private float nextWaterJump;
     private SpriteRenderer sprite;
+    private bool attackDelay;
 
-    private void Awake()
+    private IEnumerator attackCoroutine;
+
+    void Start()
     {
-        Debug.Log(File.Exists(Application.persistentDataPath + "/gamesave.save"));
-        if (File.Exists(Application.persistentDataPath + "/gamesave.save"))
-        {
-            Debug.Log("Reading Save File");
-            // 2
-            // player = GameObject.FindGameObjectWithTag("Player");
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
-            Save save = (Save)bf.Deserialize(file);
-            file.Close();
-
-            // 3
-
-            float xPosition = save.xSpawnPosition;
-            float yPosition = save.ySpawnPosition;
-            Debug.Log(xPosition);
-            Debug.Log(yPosition);
-
-            gameObject.GetComponent<Transform>().position = new Vector3(xPosition, yPosition, 0);
-            Debug.Log("Game Loaded");
-
-            //Unpause();
-        }
-        else
-        {
-            Debug.Log("No game saved!");
-        }
-    }
-
-    void Start () {
-		isJumping = false;
-		lastVelocity = new Vector2(0.0f, 0.0f);
-		canStartSprint = true;
+        isJumping = false;
+        lastVelocity = new Vector2(0.0f, 0.0f);
+        canStartSprint = true;
         sprite = GetComponent<SpriteRenderer>();
     }
 
-	void FixedUpdate () {
-		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
-        
-		horizontalMovement (rb);
-		verticalMovement (rb);
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
-	}
+        horizontalMovement(rb);
+        verticalMovement(rb);
 
-	void horizontalMovement (Rigidbody2D rb) {
-		float momentum = xSpeed;
+    }
 
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+    private void LateUpdate()
+    {
+        try
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                string heldObjectTag = gameObject.transform.GetChild(0).name;
+                if (heldObjectTag == "Sword" && !attackDelay)
+                {
+                    attackCoroutine = attack();
+                    StartCoroutine(attackCoroutine);
+                }
+            }
+        }
+        catch (UnityException e)
+        {
+            Debug.Log("Not holding an object");
+        }
+    }
 
-		//float sprintHorizontal = Input.GetAxis ("Sprint");
-		float sprintHorizontal = 0;
+    void horizontalMovement(Rigidbody2D rb)
+    {
+        float momentum = xSpeed;
 
-		if (isJumping || isInWater) {
-			//if (sprintHorizontal == 0)
-			canStartSprint = false;
-			if (rb.velocity.x == 0) {
-				momentum = 0;
-				if (Input.GetButton ("Horizontal"))
-					momentum = moveHorizontal;
-			}
-		} else
-			canStartSprint = true;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //float sprintHorizontal = Input.GetAxis ("Sprint");
+        float sprintHorizontal = 0;
+
+        if (isJumping || isInWater)
+        {
+            //if (sprintHorizontal == 0)
+            canStartSprint = false;
+            if (rb.velocity.x == 0)
+            {
+                momentum = 0;
+                if (Input.GetButton("Horizontal"))
+                    momentum = moveHorizontal;
+            }
+        }
+        else
+            canStartSprint = true;
 
         if (moveHorizontal < 0)
         {
             sprintHorizontal = -sprintHorizontal;
             sprite.flipX = false;
+            transform.localScale.Set(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             //momentum = -xSpeed;
         }
-        else if (moveHorizontal == 0) sprite.flipX = sprite.flipX;
-        else sprite.flipX = true;
-
-        
-
-		if (!(Input.GetButton("Horizontal") && canStartSprint) || isInWater)
-			sprintHorizontal = 0;
-
-		Vector2 movement = new Vector2 (
-			(moveHorizontal * momentum) + (sprintHorizontal * sprintMultiplier), 
-			rb.velocity.y);
-		
-		rb.velocity = movement;
+        else if (moveHorizontal == 0)
+        {
+            sprite.flipX = sprite.flipX;
+        }
+        else
+        {
+            sprite.flipX = true;
+            transform.localScale.Set(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
 
 
-	}
 
-	void verticalMovement(Rigidbody2D rb) {
+        if (!(Input.GetButton("Horizontal") && canStartSprint) || isInWater)
+            sprintHorizontal = 0;
 
-		float acceleration = (rb.velocity.y - lastVelocity.y) / Time.fixedDeltaTime;
-		lastVelocity = rb.velocity;
+        Vector2 movement = new Vector2(
+            (moveHorizontal * momentum) + (sprintHorizontal * sprintMultiplier),
+            rb.velocity.y);
 
-        
-		if (isJumping && -landingTolerance <= rb.velocity.y && rb.velocity.y <= landingTolerance) {
-			Debug.Log (isJumping);
-			isJumping = false;
-		}
-
-		else if (Input.GetButton("Jump")){
-			if (isInWater) {
-				if (Time.time > nextWaterJump) {
-					nextWaterJump = Time.time + (waterJumpFrames / 60f);
-					rb.velocity = new Vector2 (rb.velocity.x, 0);
-					rb.AddForce (new Vector2 (0.0f, jumpForce));
-				}
-
-			} else {
-
-				if (!isJumping && -landingTolerance <= acceleration && acceleration <= landingTolerance) {
-					
-					rb.velocity = new Vector2 (rb.velocity.x, 0);
-					rb.AddForce (new Vector2 (0.0f, jumpForce));
-					isJumping = true;
-				}
+        rb.velocity = movement;
 
 
-				else if (isJumping && acceleration < 0 && rb.velocity.y > 0) {
-					rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y * (1+accelSlowdown));
-				}
-			}
-		}
-			
-}
+    }
 
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Water") {
+    void verticalMovement(Rigidbody2D rb)
+    {
 
-			isInWater = true;
-		}
-
-	}
-
-	void OnTriggerExit2D(Collider2D other){
-		if (other.tag == "Water") {
-			isInWater = false;
-		}
-
-	}
+        float acceleration = (rb.velocity.y - lastVelocity.y) / Time.fixedDeltaTime;
+        lastVelocity = rb.velocity;
 
 
+        if (isJumping && -landingTolerance <= rb.velocity.y && rb.velocity.y <= landingTolerance)
+        {
+            Debug.Log(isJumping);
+            isJumping = false;
+        }
+
+        else if (Input.GetButton("Jump"))
+        {
+            if (isInWater)
+            {
+                if (Time.time > nextWaterJump)
+                {
+                    nextWaterJump = Time.time + (waterJumpFrames / 60f);
+                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.AddForce(new Vector2(0.0f, jumpForce));
+                }
+
+            }
+            else
+            {
+
+                if (!isJumping && -landingTolerance <= acceleration && acceleration <= landingTolerance)
+                {
+
+                    rb.velocity = new Vector2(rb.velocity.x, 0);
+                    rb.AddForce(new Vector2(0.0f, jumpForce));
+                    isJumping = true;
+                }
+
+
+                else if (isJumping && acceleration < 0 && rb.velocity.y > 0)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * (1 + accelSlowdown));
+                }
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Water")
+        {
+
+            isInWater = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Water")
+        {
+            isInWater = false;
+        }
+
+    }
+
+    private IEnumerator attack()
+    {
+        //float timeToDespawn = Time.time + (1 / 6f);
+        //while (timeToDespawn > Time.time)
+        // {
+
+        // }
+        Debug.Log("Attack Starts");
+        attackDelay = true;
+        GameObject tempHitbox = Instantiate(hitbox, gameObject.GetComponent<Transform>());
+        if (sprite.flipX)
+        {
+            tempHitbox.transform.localPosition = new Vector2(1 / 3f, 0.04f);
+        }
+        else
+        {
+            tempHitbox.transform.localPosition = new Vector2(-1 / 3f, 0.04f);
+        }
+
+        yield return new WaitForSeconds(0.4f);
+        Destroy(tempHitbox);
+
+        yield return new WaitForSeconds(0.4f);
+        attackDelay = false;
+        Debug.Log("Attack Ends");
+    }
 
 }
