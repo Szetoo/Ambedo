80c17031ed91e2bbfde90f1ac06984e508243192
Alex Pattison <alex.pattison77@gmail.com>
2019-12-23 16:52:17 -0500

    update

--- Ambedo (UNITY PROJECT)/Assets/Scripts/Tests-Alex/TestSuite.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/Tests-Alex/TestSuite.cs
@@ -4,36 +4,50 @@ using NUnit.Framework;
 using System.Collections;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TestSuite
 {
     private Scene scene;
+    bool clicked;
+    GameObject buttonObject;
 
     // 1
     [UnityTest]
     public IEnumerator MainMenuToGame()
     {
-        EditorSceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
-        yield return new WaitForEndOfFrame();
+        GameObject sceneObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Scenes/Main Menu"));
+
+        //EditorSceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
+        
         scene = EditorSceneManager.GetActiveScene();
         Debug.Log(message: "-Debug Current Scene-");
         Debug.Log(scene.name);
         Assert.True(scene.name == "Main Menu");
 
-        //buttonObject = GameObject.Find("Continue");
-        //Debug.Log(message: "-Debug Current Button-");
-        //Debug.Log(buttonObject);
-        //Button setupButton = buttonObject.GetComponent<Button>();
-        //Assert.NotNull(setupButton);
-        //clicked = false;
-        //setupButton.onClick.AddListener(Clicked);
-        //setupButton.onClick.Invoke();
-        //Assert.True(clicked);
+        buttonObject = GameObject.Find("Continue");
+        Debug.Log(message: "-Debug Current Button-");
+        Debug.Log(buttonObject);
+        Button setupButton = buttonObject.GetComponent<Button>();
+        Assert.NotNull(setupButton);
+        clicked = false;
+        setupButton.onClick.AddListener(Clicked);
+        setupButton.onClick.Invoke();
+        Assert.True(clicked);
 
-        //scene = EditorSceneManager.GetActiveScene();
-        //Debug.Log(message: "-Debug Current Scene-");
-        //Debug.Log(scene.name);
-        //Assert.True(scene.name == "Corey_Scene");
+        yield return new WaitForEndOfFrame();
+
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Corey_Scene");
+    }
+
+
+
+    private void Clicked()
+    {
+        clicked = true;
     }
 
 }
