34366b22711cd561f1db81ac82133f3a1910902b
Alex Pattison <alex.pattison77@gmail.com>
2019-12-23 16:41:36 -0500

    Tests-Alex

---
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/Tests-Alex/TestSuite.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class TestSuite
+{
+    private Scene scene;
+
+    // 1
+    [UnityTest]
+    public IEnumerator MainMenuToGame()
+    {
+        EditorSceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
+        yield return new WaitForEndOfFrame();
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Main Menu");
+
+        //buttonObject = GameObject.Find("Continue");
+        //Debug.Log(message: "-Debug Current Button-");
+        //Debug.Log(buttonObject);
+        //Button setupButton = buttonObject.GetComponent<Button>();
+        //Assert.NotNull(setupButton);
+        //clicked = false;
+        //setupButton.onClick.AddListener(Clicked);
+        //setupButton.onClick.Invoke();
+        //Assert.True(clicked);
+
+        //scene = EditorSceneManager.GetActiveScene();
+        //Debug.Log(message: "-Debug Current Scene-");
+        //Debug.Log(scene.name);
+        //Assert.True(scene.name == "Corey_Scene");
+    }
+
+}
+
