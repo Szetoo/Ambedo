73f3d05a1d7c7f262b92ba8de5c4d081041f6be2
Alex Pattison <alex.pattison77@gmail.com>
2019-12-23 17:07:16 -0500

    update

--- Ambedo (UNITY PROJECT)/Assets/Scripts/Tests-Alex/TestSuite.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/Tests-Alex/TestSuite.cs
@@ -16,34 +16,61 @@ public class TestSuite
     [UnityTest]
     public IEnumerator MainMenuToGame()
     {
-        GameObject sceneObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Scenes/Main Menu"));
-        return null;
-        //EditorSceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
+        //GameObject sceneObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Scenes/Main Menu"));
+        EditorSceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
         
-        //scene = EditorSceneManager.GetActiveScene();
-        //Debug.Log(message: "-Debug Current Scene-");
-        //Debug.Log(scene.name);
-        //Assert.True(scene.name == "Main Menu");
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Main Menu");
 
-        //buttonObject = GameObject.Find("Continue");
-        //Debug.Log(message: "-Debug Current Button-");
-        //Debug.Log(buttonObject);
-        //Button setupButton = buttonObject.GetComponent<Button>();
-        //Assert.NotNull(setupButton);
-        //clicked = false;
-        //setupButton.onClick.AddListener(Clicked);
-        //setupButton.onClick.Invoke();
-        //Assert.True(clicked);
+        buttonObject = GameObject.Find("Continue");
+        Debug.Log(message: "-Debug Current Button-");
+        Debug.Log(buttonObject);
+        Button setupButton = buttonObject.GetComponent<Button>();
+        Assert.NotNull(setupButton);
+        clicked = false;
+        setupButton.onClick.AddListener(Clicked);
+        setupButton.onClick.Invoke();
+        Assert.True(clicked);
 
-        //yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
 
-        //scene = EditorSceneManager.GetActiveScene();
-        //Debug.Log(message: "-Debug Current Scene-");
-        //Debug.Log(scene.name);
-        //Assert.True(scene.name == "Corey_Scene");
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Corey_Scene");
     }
 
+    // 2
+    [UnityTest]
+    public IEnumerator GameToMainMenu()
+    {
+        // Test 'Quit to Main Menu' button changes scene from pause menu to game
+        EditorSceneManager.LoadScene("Assets/Scenes/Corey_Scene.unity");
 
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Corey_Scene");
+
+        buttonObject = GameObject.Find("Quit To Main Menu");
+        Debug.Log(message: "-Debug Current Button-");
+        Debug.Log(buttonObject);
+        Button setupButton = buttonObject.GetComponent<Button>();
+        Assert.NotNull(setupButton);
+        clicked = false;
+        setupButton.onClick.AddListener(Clicked);
+        setupButton.onClick.Invoke();
+        Assert.True(clicked);
+
+        yield return new WaitForEndOfFrame();
+
+        scene = EditorSceneManager.GetActiveScene();
+        Debug.Log(message: "-Debug Current Scene-");
+        Debug.Log(scene.name);
+        Assert.True(scene.name == "Main Menu");
+    }
 
     private void Clicked()
     {
