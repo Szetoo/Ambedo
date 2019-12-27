e4e7c8d98e92a09f8c0eea012e30f8a404c8d89c
Corey Szeto <szetoc@mcmaster.ca>
2019-12-27 17:15:59 -0500

    changes

--- Ambedo (UNITY PROJECT)/Assets/Scripts/PlayerMovementController.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/PlayerMovementController.cs
@@ -36,19 +36,45 @@ public class PlayerMovementController : MonoBehaviour
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
-        horizontalMovement(rb);
-        verticalMovement(rb);
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        bool jumpButton = Input.GetButton("Jump");
 
+        //Runs when pressing a button that moves left or right
+        if (horizontalAxis != 0)
+        {
+            horizontalMovement(rb, horizontalAxis);
+
+        }
+
+        //Stops the player if they're still moving after they shouldn't be 
+        else if (rb.velocity.x != 0)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //Falling physics of player. MUST RUN BEFORE JUMP CALL.
+        if (!jumpButton)
+        {
+            verticalMovement(rb);
+
+        }
+
+        //Runs only when the jump button is being pressed/held
+        else
+        {
+            jump(rb, jumpButton);
+
+        }
     }
 
     private void LateUpdate()
     {
         try
         {
-            if (Input.GetButtonDown("Attack"))
+            if (Input.GetMouseButtonDown(0))
             {
                 string heldObjectTag = gameObject.transform.GetChild(0).name;
-                if ((heldObjectTag == "Sword" | heldObjectTag == "Sword2") && !attackDelay)
+                if (heldObjectTag == "Sword" && !attackDelay)
                 {
                     attackCoroutine = attack();
                     StartCoroutine(attackCoroutine);
@@ -61,15 +87,19 @@ public class PlayerMovementController : MonoBehaviour
         }
     }
 
-    void horizontalMovement(Rigidbody2D rb)
+    public void horizontalMovement(Rigidbody2D rb, float moveHorizontal)
     {
+        /*
+         * Note: Make sure sprintHorizontal stays at 0. Even though we don't use sprinting, this function requires
+         * a lot of checks of this variable.
+        */
         float momentum = xSpeed;
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
 
         //float sprintHorizontal = Input.GetAxis ("Sprint");
         float sprintHorizontal = 0;
 
+
+        //Water physics. Irrelevant to us for now.
         if (isJumping || isInWater)
         {
             //if (sprintHorizontal == 0)
@@ -84,6 +114,7 @@ public class PlayerMovementController : MonoBehaviour
         else
             canStartSprint = true;
 
+        //Moving to the left
         if (moveHorizontal < 0)
         {
             sprintHorizontal = -sprintHorizontal;
@@ -91,10 +122,14 @@ public class PlayerMovementController : MonoBehaviour
             transform.localScale.Set(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             //momentum = -xSpeed;
         }
+
+        //Stationary (I'm particularly worried that this won't work, but I can't test it)
         else if (moveHorizontal == 0)
         {
             sprite.flipX = sprite.flipX;
         }
+
+        //Moving to the right
         else
         {
             sprite.flipX = true;
@@ -102,10 +137,11 @@ public class PlayerMovementController : MonoBehaviour
         }
 
 
-
+        //More sprinting stuff
         if (!(Input.GetButton("Horizontal") && canStartSprint) || isInWater)
             sprintHorizontal = 0;
 
+        //Create the movement vector and apply it to the rigidBody2D
         Vector2 movement = new Vector2(
             (moveHorizontal * momentum) + (sprintHorizontal * sprintMultiplier),
             rb.velocity.y);
@@ -115,53 +151,58 @@ public class PlayerMovementController : MonoBehaviour
 
     }
 
-    void verticalMovement(Rigidbody2D rb)
+    public void verticalMovement(Rigidbody2D rb)
     {
 
         float acceleration = (rb.velocity.y - lastVelocity.y) / Time.fixedDeltaTime;
         lastVelocity = rb.velocity;
 
-
         if (isJumping && -landingTolerance <= rb.velocity.y && rb.velocity.y <= landingTolerance)
         {
-            Debug.Log(isJumping);
             isJumping = false;
         }
 
-        else if (Input.GetButton("Jump"))
-        {
-            if (isInWater)
-            {
-                if (Time.time > nextWaterJump)
-                {
-                    nextWaterJump = Time.time + (waterJumpFrames / 60f);
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
-                    rb.AddForce(new Vector2(0.0f, jumpForce));
-                }
-
-            }
-            else
-            {
-
-                if (!isJumping && -landingTolerance <= acceleration && acceleration <= landingTolerance)
-                {
-
-                    rb.velocity = new Vector2(rb.velocity.x, 0);
-                    rb.AddForce(new Vector2(0.0f, jumpForce));
-                    isJumping = true;
-                }
-
-
-                else if (isJumping && acceleration < 0 && rb.velocity.y > 0)
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * (1 + accelSlowdown));
-                }
-            }
-        }
 
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    public void jump(Rigidbody2D rb, bool jump)
+    {
+        float acceleration = (rb.velocity.y - lastVelocity.y) / Time.fixedDeltaTime;
+        lastVelocity = rb.velocity;
+
+        //Water physics stuff
+        if (isInWater)
+        {
+            if (Time.time > nextWaterJump)
+            {
+                nextWaterJump = Time.time + (waterJumpFrames / 60f);
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                rb.AddForce(new Vector2(0.0f, jumpForce));
+            }
+
+        }
+
+        else
+        {
+            //Executes if player is eligible to jump
+            if (!isJumping && -landingTolerance <= acceleration && acceleration <= landingTolerance)
+            {
+                Debug.Log("Jump!");
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                rb.AddForce(new Vector2(0.0f, jumpForce));
+                isJumping = true;
+            }
+
+            //Executes if player is already jumping
+            else if (isJumping && acceleration < 0 && rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * (1 + accelSlowdown));
+                Debug.Log("Holding Jump");
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Water")
         {
@@ -171,7 +212,7 @@ public class PlayerMovementController : MonoBehaviour
 
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    public void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Water")
         {
@@ -180,7 +221,7 @@ public class PlayerMovementController : MonoBehaviour
 
     }
 
-    private IEnumerator attack()
+    public IEnumerator attack()
     {
         //float timeToDespawn = Time.time + (1 / 6f);
         //while (timeToDespawn > Time.time)
