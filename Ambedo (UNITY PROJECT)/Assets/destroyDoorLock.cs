7164c130c417aa37b71433a09050dc4fc73231f4
Limng Liu <liul21@mcmaster.ca>
2020-02-29 20:06:52 -0500

    two puzzles update

---
+++ Ambedo (UNITY PROJECT)/Assets/destroyDoorLock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyDoorLock : MonoBehaviour
+{
+
+    public GameObject lockCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "rockBreakDoor")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Destroy(lockCollider);
+    }
+}
---
+++ Ambedo (UNITY PROJECT)/Assets/trigger_switch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigger_switch : MonoBehaviour
+{
+
+    public GameObject activeObject;
+    public GameObject deactiveLockObject;
+    public GameObject lockObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        activeObject.SetActive(true);
+        Destroy(lockObject);
+
+        deactiveLockObject.SetActive(false);
+    }
+}
+
