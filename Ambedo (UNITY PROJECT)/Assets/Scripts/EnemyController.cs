649930851601c4f50f65ee87972250211af1f39d
Thomas Dykstra <dykstrat@mcmaster.ca>
2020-04-07 13:43:58 -0400

    New enemies + moving platforms

--- Ambedo (UNITY PROJECT)/Assets/Scripts/EnemyController.cs
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/EnemyController.cs
@@ -2,74 +2,266 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
-{
+public class EnemyController : MonoBehaviour {
 
+    public enum MovementType { moveTowardsPlayer, moveAwayFromPlayer, flying, shootProjectiles, stationary, selfDefense, terrifiedOfDying };
+    public MovementType ai;
+
+    public float projectileSpeed;
+    public float projectileFireRate;
+
+    public BoxCollider2D territory;
     GameObject player;
     private bool playerNearby;
 
-
-    private float speed = 2.5f;
+    public GameObject enemy;
+    public GameObject projectileSpawner;
+    public float speed = 2;
     private Rigidbody2D rbdy;
+    private int moveMode;
+    private Vector2 pos;
 
-    void Start()
+    private bool flyingIdle;
+    private bool isFacingRight;
+
+    private void Awake()
     {
+        switch (ai)
+        {
+            case MovementType.moveTowardsPlayer:
+                moveMode = 1;
+                break;
+            case MovementType.moveAwayFromPlayer:
+                moveMode = 2;
+                break;
+            case MovementType.flying:
+                moveMode = 3;
+                flyingIdle = true;
+                isFacingRight = true;
+                break;
+            case MovementType.shootProjectiles:
+                moveMode = 4;
+                break;
+            case MovementType.stationary:
+                moveMode = 5;
+                break;
+            case MovementType.selfDefense:
+                moveMode = 6;
+                break;
+            case MovementType.terrifiedOfDying:
+                moveMode = 7;
+                break;
+        }
+    }
+
+    // Use this for initialization
+    void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
         playerNearby = false;
         rbdy = gameObject.GetComponent<Rigidbody2D>();
+        //rbdy.velocity = Vector2.zero;
+        if (moveMode == 3)
+        {
+            rbdy.constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+        if (moveMode == 4)
+        {
+            //projectileSpawner.transform.position = new Vector3(-127, 0);
+            GameObject spawner = Instantiate(projectileSpawner);
+            spawner.transform.parent = gameObject.transform;
+            spawner.transform.localPosition = Vector2.zero;
+            spawner.transform.localRotation = gameObject.transform.rotation;
+            spawner.GetComponent<ProjectileSpawnerController>().fireRate = projectileFireRate;
+        }
+        
+        pos = gameObject.transform.position;
     }
 
-    void Update()
-    {
+    // Update is called once per frame
+    void Update() {
         player = GameObject.FindGameObjectWithTag("Player");
         playerNearby = transform.GetChild(0).GetComponent<EnemyAggroZone>().playerNearby;
-
-        //If enemy is alive and player is nearby, move to player
-        if (playerNearby & player != null & gameObject.GetComponent<Animator>().GetBool("Alive") == true)
-        {
-            MoveToPlayer();
-        }
-        else
+        bool isAlive = gameObject.GetComponent<Animator>().GetBool("Alive");
+        if (!isAlive)
         {
             Rest();
         }
+        else
+        {
+            switch (moveMode)
+            {
+                case 1:
+                    if (playerNearby & player != null & isAlive)
+                    {
+                        MoveToPlayer();
+                    }
+                    else
+                    {
+                        Rest();
+                    }
+                    break;
+                case 2:
+                    if (playerNearby & player != null & isAlive)
+                    {
+                        MoveAwayFromPlayer();
+                    }
+                    else
+                    {
+                        Rest();
+                    }
+                    break;
+                case 3:
+                    if (playerNearby & player != null & isAlive)
+                    {
+                        flyingIdle = false;
+                        MoveDown();
+                    }
+                    else if (flyingIdle)
+                    {
+                        if (transform.position.x < pos.x - 1)
+                        {
+
+                            isFacingRight = true;
+                        }
+                        else if (transform.position.x > pos.x + 1)
+                        {
+                            isFacingRight = false;
+                        }
+
+                        if (isFacingRight)
+                        {
+                            MoveRight();
+                        }
+                        else
+                        {
+                            MoveLeft();
+                        }
+                    }
+                    else
+                    {
+                        MoveUp();
+                        if (gameObject.GetComponent<Transform>().transform.position.y >= pos.y)
+                        {
+                            gameObject.GetComponent<Transform>().transform.position = pos;
+                            flyingIdle = true;
+                        }
+
+
+                    }
+                    break;
+                case 4:
+                    if (gameObject.GetComponent<SpriteRenderer>().flipX)
+                    {
+                        transform.GetChild(1).SetPositionAndRotation(transform.position, Quaternion.identity);
+                    }
+                    else
+                    {
+                        transform.GetChild(1).SetPositionAndRotation(transform.position, new Quaternion(0, 0, 180, 1));
+                    }
+
+                    Rest();
+                    break;
+                case 5:
+                    Rest();
+                    break;
+                case 6:
+                    if (playerNearby & player != null & isAlive & gameObject.GetComponent<EnemyHealth>().currentHP < gameObject.GetComponent<EnemyHealth>().maxHP)
+                    {
+                        gameObject.GetComponentInParent<Animator>().SetBool("Hurt", true);
+                        speed = Mathf.Min(speed * 1.02f, player.GetComponent<PlayerMovementController>().xSpeed);
+                        MoveToPlayer();
+                    }
+                    else
+                    {
+                        gameObject.GetComponentInParent<Animator>().SetBool("Hurt", false);
+                        speed = 2;
+                        Rest();
+                    }
+                    break;
+                case 7:
+                    if (playerNearby & player != null & isAlive & gameObject.GetComponent<EnemyHealth>().currentHP < gameObject.GetComponent<EnemyHealth>().maxHP)
+                    {
+                        gameObject.GetComponentInParent<Animator>().SetBool("Hurt", true);
+                        speed = Mathf.Min(speed * 1.02f, player.GetComponent<PlayerMovementController>().xSpeed);
+                        MoveAwayFromPlayer();
+                    }
+                    else
+                    {
+                        gameObject.GetComponentInParent<Animator>().SetBool("Hurt", false);
+                        speed = 2;
+                        Rest();
+                    }
+                    break;
+            }
+        }
     }
 
-    private void MoveToPlayer()
-    {
-        rbdy.velocity = new Vector2(0, rbdy.velocity.y);
 
-        //Move towards and face (by flipping sprite) player
+
+
+
+     private void MoveToPlayer()
+    {
+
+        //move towards and face player
         gameObject.GetComponent<AudioSource>().enabled = true;
 
         if (transform.position.x - player.transform.position.x > 1)
         {
             transform.gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            transform.Translate(new Vector2(-1 * speed * Time.deltaTime, 0));
+            transform.Translate(new Vector2(-1*speed * Time.deltaTime, 0));
         }
-        else if (transform.position.x - player.transform.position.x < -1)
+        if (transform.position.x - player.transform.position.x < -1)
         {
             transform.gameObject.GetComponent<SpriteRenderer>().flipX = true;
             transform.Translate(new Vector2(speed * Time.deltaTime, 0));
         }
-        /*
-        if (transform.position.x - player.transform.position.x > 1)
-        {
-           
-            transform.Translate(new Vector2(-1 * speed * Time.deltaTime, 0));
-        }
-        else if (transform.position.x - player.transform.position.x < -1)
-        {
-            
-            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
-        }*/
-        //yield return new WaitForSeconds(0f);
     }
 
-    //Rest if not nearby
+    //rest if not nearby
     private void Rest()
     {
-        rbdy.velocity = new Vector2(0, rbdy.velocity.y);
+        rbdy.velocity = Vector2.zero;
+       // rbdy.Sleep();
         gameObject.GetComponent<AudioSource>().enabled = false;
+    } 
+
+    private void MoveAwayFromPlayer()
+    {
+        gameObject.GetComponent<AudioSource>().enabled = true;
+
+        if (transform.position.x - player.transform.position.x < -1)
+        {
+            transform.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            transform.Translate(new Vector2(-1 * speed * Time.deltaTime, 0));
+            
+        }
+        else if (transform.position.x - player.transform.position.x > 1)
+        {
+            transform.gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+           
+        }
+    }
+
+    private void MoveUp()
+    {
+        transform.Translate(transform.up * 2 * speed * Time.deltaTime);
+    }
+
+    private void MoveDown()
+    {
+        gameObject.GetComponent<AudioSource>().enabled = true;
+        transform.Translate(-transform.up*4*speed*Time.deltaTime);
+    }
+
+    private void MoveLeft()
+    {
+        transform.Translate(-transform.right * speed * Time.deltaTime);
+    }
+
+    private void MoveRight()
+    {
+        transform.Translate(transform.right * speed * Time.deltaTime);
     }
 }
---
+++ Ambedo (UNITY PROJECT)/Assets/Scripts/PlatformMovementController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementController : MonoBehaviour
+
+    
+{
+    
+    public enum MovementType { horizontal, vertical, diagonal };
+    public MovementType movementType;
+
+    public Vector2 speed;
+    public float travelTime;
+    public float waitTime;
+
+    private Vector3 position;
+    private IEnumerator toUse;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        switch (movementType)
+        {
+            case MovementType.horizontal:
+                speed.y = 0;
+                break;
+            case MovementType.vertical:
+                speed.x = 0;
+                break;
+            case MovementType.diagonal:
+                break;
+            default:
+                Debug.Log("No movement type selected");
+                break;
+
+        }
+    }
+
+    void Start()
+    {
+        toUse = movementCoroutine();
+        StartCoroutine(toUse);
+        
+    }
+
+
+    private IEnumerator movementCoroutine()
+    {
+        while (true) {
+            float timeUntilStop = Time.time + travelTime;
+            while (Time.time < timeUntilStop)
+            {
+                gameObject.GetComponent<Transform>().Translate(new Vector3(speed.x * Time.fixedDeltaTime, speed.y * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+
+            }
+
+
+            yield return new WaitForSeconds(waitTime);
+
+            timeUntilStop = Time.time + travelTime;
+            while (Time.time < timeUntilStop)
+            {
+                gameObject.GetComponent<Transform>().Translate(new Vector3(-speed.x * Time.fixedDeltaTime, -speed.y * Time.fixedDeltaTime));
+                yield return new WaitForEndOfFrame();
+
+            }
+
+            yield return new WaitForSeconds(waitTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.collider.transform.SetParent(null);
+        }
+    }
+
+
+}
